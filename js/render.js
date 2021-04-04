c9bec9846ddd83eacd4f3dafd48b861d27b1b2ca
jehyeon <s85737@gmail.com>
2021-04-04 18:37:12 +0900

    이미지 및 json 추가, 상품 상세정보 1차완성

--- js/render.js
+++ js/render.js
@@ -40,7 +40,7 @@ function renderProduct(model, length, JSONKey, condition) {
                 itemLiA.setAttribute("name", DB.itemNumber);
                 itemLiA.innerText = DB.name;
                 itemLiB.setAttribute("class", "b");
-                itemLiB.innerText = DB.price;
+                itemLiB.innerText = DB.price + "원";
                 itemLiC.setAttribute("class", "c");
                 itemAHref.href = "detail_view.html";
 
@@ -105,6 +105,72 @@ if(getCookie("clickId")===null){
     alert('잘못된 접근입니다.');
     window.location.href = "index.html"
 }else{
+
+    let localItemDB = (JSON.parse(localStorage.getItem("DB"))).DB;
+
+    let productThumb = document.getElementById("productThumb");
+    let productText = document.getElementById("productText");
+    let productTable = document.getElementById("productTable");
+    let productPrice = document.getElementById("productPrice");
+    let productImages = document.getElementById("productImages");
+
+    let itemNumber = getCookie("clickId");
+    let Item = localItemDB.items[itemNumber];
+    let itemName = Item.name;
+    let itemPrice = Item.price;
+    let itemThumbSrc = Item.image1;
+    let itemImageSrc2 = Item.image2;
+    let itemImageSrc3 = Item.image3;
+    let itemDetail = Item.detail;
+
+    let itemNameH3 = document.createElement("h3");
+    itemNameH3.innerText = itemName;
+    let itemDetailP = document.createElement("p");
+    itemDetailP.innerText = itemDetail;
+
+    productText.appendChild(itemNameH3);
+    productText.appendChild(itemDetailP);
+
+    let itemThumb = document.createElement("img");
+    itemThumb.src = "../css/image/" + itemThumbSrc + ".jpg";
+    itemThumb.width = 500;
+    itemThumb.height = 750;
+    productThumb.appendChild(itemThumb);
+
+    // let itemPriceTotal = document.createElement("p");
+    // itemPriceTotal.innerHTML = "총 상품금액 <span>"+ itemPrice +"</span>원";
+    // productPrice.appendChild(itemPriceTotal);
+
+
+    let itemPriceTd = document.createElement("td");
+    itemPriceTd.innerText = itemPrice + "원";
+    productTable.appendChild(itemPriceTd);
+
+    let itemImage1 = document.createElement("img");
+    itemImage1.src = "../css/image/" + itemThumbSrc + ".jpg";
+    itemImage1.width = 500;
+    itemImage1.height = 750;
+
+    let itemImage2 = document.createElement("img");
+    itemImage2.src = "../css/image/" + itemImageSrc2 + ".jpg";
+    itemImage2.width = 500;
+    itemImage2.height = 750;
+
+    let itemImage3 = document.createElement("img");
+    itemImage3.src = "../css/image/" + itemImageSrc3 + ".jpg";
+    itemImage3.width = 500;
+    itemImage3.height = 750;
+
+    productImages.appendChild(itemImage1);
+    productImages.appendChild(itemImage2);
+    productImages.appendChild(itemImage3);
+
+
+
+
+
+
+
 
 }
 
