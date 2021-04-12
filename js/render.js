ec4f622c82ce1152eb1b41c230bb42cec4d7abca
jehyeon <s85737@gmail.com>
2021-04-12 17:43:04 +0900

    no message

--- js/render.js
+++ js/render.js
@@ -43,8 +43,8 @@ function renderProduct(model, length, JSONKey, condition) {
                 itemInput.setAttribute("name", "chooseItem");
                 itemInput.setAttribute("value", DB.itemNumber)
                 imgSrc.src = "../css/image/" + DB.image1 + ".jpg";
-                imgSrc.width = 200;
-                imgSrc.height = 270;
+                imgSrc.width = 220;
+                imgSrc.height = 350;
                 imgSrc.name = DB.itemNumber;
                 itemLiA.setAttribute("class", "a");
                 itemLiA.setAttribute("name", DB.itemNumber);
